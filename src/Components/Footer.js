a2df39b5e9b52dcc808409593bdb2e5cf86f3f99
Pallavi687 <pallavikhadse91@gmail.com>
2023-08-08 14:51:14 +0530

    .

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+// import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
 import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
